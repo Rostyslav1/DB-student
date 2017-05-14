a68d4a9c15c5f3479f391093617d00e9475360ce
Rostyslav Hanych <rostyslavweb@gmail.com>
2017-05-14 20:40:47 +0300

    Primary commin

---
+++ Student DB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Student_DB.Startup))]
+namespace Student_DB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
